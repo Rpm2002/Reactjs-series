cbfe1eedb22fc8b60c058d4d02bf17f274487918
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2024-01-05 09:58:43 +0000

    made some changes in app.jsx and main.jsx

--- 01_Vite_React/src/App.jsx
+++ 01_Vite_React/src/App.jsx
@@ -2,12 +2,15 @@ import Chai from "./Chai"
 
 function App() {
 
+  const username='Chai aur code'
   return (
     // 1 hi element return kar sakte hai isliye <> </> k andar sab wrap kar diya taaki wo pura 1 hi count hoga
+    // {username} ye 1 expression hai yaha puri JS nhi likhte bas jo JS ka final outccome jo
+    // evaluate ho chuka hai wo likhte hai
     <> 
       <Chai/>
-      <h1>Waah bete mauj kardi</h1>
-      </>
+      <h1>Waah bete mauj {username} kardi</h1> 
+    </>
   )
 }
 
--- 01_Vite_React/src/main.jsx
+++ 01_Vite_React/src/main.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement={
+//   type:'a',
+//   props:{
+//       href:'http://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotherElement=(
+  <a href="http://google.com" target='_blank'>Click to visit GOOGLE</a>
+)
+  
+const anotheruser='chai aur react'
+
+const ReactElement=React.createElement(
+  'a',
+  {href:'http://google.com',target:'_blank'},
+  'Click me to visit google ',
+  anotheruser // ye vealuated expressoin hai
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    // anotherElement
+    ReactElement
+    // <App/>
 )
