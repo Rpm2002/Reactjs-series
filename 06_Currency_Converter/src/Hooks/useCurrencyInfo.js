d8a2cee78ef8e5710a448cfa678da83594203274
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2024-01-13 06:55:12 +0000

    done something left but

--- 06_Currency_Converter/src/Hooks/useCurrencyInfo.js
+++ 06_Currency_Converter/src/Hooks/useCurrencyInfo.js
@@ -1,8 +1,8 @@
 import { useEffect,useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data,setData]=useState({})
     useEffect(()=>{
-        const [data,setData]=useState({})
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1
         /latest/currencies/${currency}.json`).
         then((res)=>res.json()).
