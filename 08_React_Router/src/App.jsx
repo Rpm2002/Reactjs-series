206e232c509dcdad2b275fda14471c9e0ace3cff
Rhythm <rhythm758002@gmail.com>
2024-01-19 15:18:14 +0530

    Made a small project of react router

---
+++ 08_React_Router/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
