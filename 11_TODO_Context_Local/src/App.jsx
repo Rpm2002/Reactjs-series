c1b7cbbf5de14c128636e23cc79e80a151745e33
Rhythm <rhythm758002@gmail.com>
2024-01-23 14:15:27 +0530

    Todo added more functionality

--- 11_TODO_Context_Local/src/App.jsx
+++ 11_TODO_Context_Local/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './Context'
 
@@ -11,9 +11,31 @@ function App() {
 
   const updateTodo=(todo,id)=>{
     setTodos((prev)=>prev.map((prevTodo)=>(
-      prevTodo.id===id? todo:prevTodo
-    )))
+      prevTodo.id===id? todo:prevTodo)))
   }
+
+  const deleteTodo=(id)=>{
+    setTodos((prev)=>prev.filter((todo)=>todo.id!=id))
+  }
+
+  const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>
+    prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
+  }
+
+  useEffect(() => {
+    const todos=JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length>0)
+    {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(()=>{
+   localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+  
 
   return (
     <TodoProvider value={{todos,addTodo,deleteTodo,updateTodo,toggleComplete}}>
